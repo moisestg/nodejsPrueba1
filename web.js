0171e9fc0b724ab0f11f035ee87a7637f66b7142
Moisés Torres <moises@moises-VirtualBox.(none)>
2013-07-15 15:56:28 +0200

    menos mal

--- web.js
+++ web.js
@@ -14,10 +14,17 @@ var miFuncion = function(request, response) {
 //A continuación recuperamos la info que nos proporciona la api "os" y la incorporamos a un html
 	
   var html = "<html><head><title>Ejemplo NodeJS y Heroku</title><meta charset='utf-8'></    head><body><h3>Información de las máquinas del servidor de Heroku</h3><ol>";
-  var tmpdir = os.tmpdir();
-  html+= "<li>Directorio por defecto para archivos temporales: "+tmpdir+"</li></ol></body></html>";
-
   
+  var tmpdir = os.tmpdir();
+  html+= "<li>Directorio por defecto para archivos temporales: "+tmpdir+"</li>";
+  
+  var endianness = os.endianness();
+  html+= "<li>CPU trabaja en Big Endian (BE) o Little Endian (LE): "+endianness+"</li>";
+  
+  var hostname = os.hostname();
+  html+= "<li>Hostname del SO: "+hostname+"</li></ol></body></html>";
+
+
   response.send(html);
   
 };
