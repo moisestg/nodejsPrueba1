25b6384cbe5bb8833674eed978f5e1c864093d37
Moisés Torres <moises@moises-VirtualBox.(none)>
2013-07-15 15:24:31 +0200

    primer error subida

--- web.js
+++ web.js
@@ -13,8 +13,8 @@ var miFuncion = function(request, response) {
 
 //A continuación recuperamos la info que nos proporciona la api "os" y la incorporamos a un html
 	var resultadoCPU = os.cpus();
-  var html = "<html><head><title>Ejemplo NodeJS y Heroku</title><meta name="tipo_contenido" 		  content="text/html;" http-equiv="content-type" charset="utf-8"></head><body><h3>Información de las máquinas del servidor de Heroku</h3>";
-  html += resultadoCPU +"</body></html>";
+  var html = "<html><head><title>Ejemplo NodeJS y Heroku</title><meta name="tipo_contenido" 		  content="text/html;" http-equiv="content-type" charset="utf-8"></head><body><h3>Información de las máquinas del servidor de Heroku</h3></body></html>";
+ 
 
   
   response.send(html);
