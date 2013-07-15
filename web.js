b76cc850b91d7a6dfecedb4a69faef04f0bf1afc
Moisés Torres <moises@moises-VirtualBox.(none)>
2013-07-15 15:33:22 +0200

    s

--- web.js
+++ web.js
@@ -14,7 +14,7 @@ var miFuncion = function(request, response) {
 //A continuación recuperamos la info que nos proporciona la api "os" y la incorporamos a un html
 	var resultadoCPU = os.cpus();
   var html = "<html><head><title>Ejemplo NodeJS y Heroku</title><meta charset='utf-8'></head><body><h3>Información de las máquinas del servidor de Heroku</h3></body></html>";
- 
+  html+= resultadoCPU;
 
   
   response.send(html);
