9c6ff8615cfb8fb21fa0594a56d890f77744eff9
Moisés Torres <moises@moises-VirtualBox.(none)>
2013-07-16 15:17:04 +0200

    prueba

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ var fs = require('fs');
 
 //var int  =setInterval(function(){xmlDinamico()},3000);
 
-fs.appendFileSync('prueba.xml',"<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><usuarios><usuario><apellido>Antúnez</apellido><login>ana</login><nombre>Ana</nombre><pais><capital>Washington D.C.</capital><iso>USA</iso><moneda>Dolar</moneda><nombre>Estados Unidos de América</nombre></pais><password>a</password></usuario><usuario><apellido>Domínguez</apellido><login>david</login><nombre>David</nombre><pais><capital>Beijing</capital><iso>CHN</iso><moneda>Yuan chino</moneda><nombre>China</nombre></pais><password>d</password></usuario><usuario><apellido>Jiménez</apellido><login>juan</login><nombre>Juan</nombre><pais><capital>Londres</capital><iso>GBR</iso><moneda>Libra esterlina</moneda><nombre>Reino Unido</nombre></pais><password>j</password></usuario><usuario><apellido>Martínez</apellido><login>marta</login><nombre>Marta</nombre><pais><capital>Madrid</capital><iso>ES</iso><moneda>Euro</moneda><nombre>España</nombre></pais><password>m</password></usuario><usuario><apellido>Torres</apellido><login>moi</login><nombre>Moisés</nombre></usuario><usuario><apellido>Pérez</apellido><login>pedro</login><nombre>Pedro</nombre><pais><capital>Madrid</capital><iso>ES</iso><moneda>Euro</moneda><nombre>España</nombre></pais><password>p</password></usuario></usuarios>");
+fs.appendFileSync('prueba.xml',"<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><autor><nombre>Moisés</nombre></autor>");
 
 //var xmlDinamico = function(){
 	
