63a3e1e10d63b53e3c843a4d0c3485962a6f7a4a
Moisés Torres <moises@moises-VirtualBox.(none)>
2013-07-16 22:37:05 +0200

    moi

--- web.js
+++ web.js
@@ -37,7 +37,7 @@ var miFuncion = function(request, response) {
 
   console.log("hemos recibido algo");
 
-  //Si quito 2º parámetro (encoding) al entrar en la web me deja descargar el xml perfect y si pongo  utf-8 no sale el texto como xml. Había que usar response.set()!!!
+  //Si quito 2º parámetro (encoding) al entrar en la web me deja descargar el xml perfect y si pongo  utf-8 no sale el texto como xml. Había que usar response.set()!!!!
 
   fs.readFile('medidas.xml', function (err, data) {
   if (err) throw err;
