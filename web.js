461f517a07e1ebb8f6fa6495a70ac5d98c9e033f
Moisés Torres <moises@moises-VirtualBox.(none)>
2013-07-17 11:08:12 +0200

    probando rest

--- web.js
+++ web.js
@@ -25,6 +25,26 @@ console.log("iniciando la aplicacion");
 
 var int2=setInterval(function(){jsonvar()},3000);
 function jsonvar(){
+	var uptime = os.uptime();
+	var totalmem = os.totalmem();
+	var freemem = os.freemem();
+	var cpus = os.cpus();
+	var cpuString = "<cputimes>";
+	
+	for(var i=0; i<cpus.length;i++){
+	
+	cpuString += "<user>"+cpus[i]["times"]["user"]+"</user><nice>"+cpus[i]["times"]["nice"]+"</nice><sys>"+cpus[i]["times"]["sys"]+"</sys><idle>"+cpus[i]["times"]["idle"]+"</idle><irq>"+cpus[i]["times"]["irq"]+"</irq>";   
+  
+}
+	cpuString += "</cputimes>";	
+
+	var stringNew = string.replace("</medidas>","<medida><uptime>"+uptime+"</uptime><totalmem>"+totalmem+"</totalmem><freemem>"+freemem+"</freemem>"+cpuString+"</medida></medidas>");
+	
+	string=stringNew;
+	fs.writeFileSync('medidas.xml',stringNew);
+
+
+
  int_network2=os.networkInterfaces();
    json+=",{\"Freememory\":"+os.freemem()+",\"TotalMemory\":"+os.totalmem()+",\"uptime\":"+os.uptime()+",\"cputimes\":{";
  
@@ -57,29 +77,10 @@ var mi_funcion= function(request, response){
 //Acaba código para json~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 //Empieza código para xml~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-var int  =setInterval(function(){xmlDinamico()},3000);
+
 
 var string = "<?xml version=\"1.0\" standalone=\"yes\"?><medidas></medidas>"
 
-var xmlDinamico = function(){
-	var uptime = os.uptime();
-	var totalmem = os.totalmem();
-	var freemem = os.freemem();
-	var cpus = os.cpus();
-	var cpuString = "<cputimes>";
-	
-	for(var i=0; i<cpus.length;i++){
-	
-	cpuString += "<user>"+cpus[i]["times"]["user"]+"</user><nice>"+cpus[i]["times"]["nice"]+"</nice><sys>"+cpus[i]["times"]["sys"]+"</sys><idle>"+cpus[i]["times"]["idle"]+"</idle><irq>"+cpus[i]["times"]["irq"]+"</irq>";   
-  
-}
-	cpuString += "</cputimes>";	
-
-	var stringNew = string.replace("</medidas>","<medida><uptime>"+uptime+"</uptime><totalmem>"+totalmem+"</totalmem><freemem>"+freemem+"</freemem>"+cpuString+"</medida></medidas>");
-	
-	string=stringNew;
-	fs.writeFileSync('medidas.xml',stringNew);
-}      
 
 
 
