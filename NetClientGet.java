a207a120ccf88519d48176dad9d73c4516a85ec4
Moisés Torres <moises@moises-VirtualBox.(none)>
2013-07-22 19:11:32 +0200

    Cliente REST en Java // Moisés

---
+++ NetClientGet.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+ 
+public class NetClientGet {
+ 
+	// http://localhost:8080/labo7-completo/rest/usuarios
+	public static void main(String[] args) {
+ 
+	  try {
+ 
+		URL url = new URL("https://api.twitter.com/1.1/statuses/mentions_timeline");
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestMethod("GET");
+		conn.setRequestProperty("Accept", "application/json");
+ 
+		if (conn.getResponseCode() != 200) {
+			throw new RuntimeException("Failed : HTTP error code : "
+					+ conn.getResponseCode());
+		}
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(
+			(conn.getInputStream())));
+ 
+		String output;
+		System.out.println("Output from Server .... \n");
+		while ((output = br.readLine()) != null) {
+			System.out.println(output);
+		}
+ 
+		conn.disconnect();
+ 
+	  } catch (MalformedURLException e) {
+ 
+		e.printStackTrace();
+ 
+	  } catch (IOException e) {
+ 
+		e.printStackTrace();
+ 
+	  }
+ 
+	}
+ 
+}
